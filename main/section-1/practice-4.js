91819456ed6570c41c2642bd5ed63bd854fc517a
313183373 <313183373@qq.com>
2017-08-16 18:24:17 +0800

    finished

--- main/section-1/practice-4.js
+++ main/section-1/practice-4.js
@@ -1,21 +1,5 @@
 'use strict';
 
-function includes(collection, ch) {
-    for (let item of collection) {
-        if (item === ch) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
 module.exports = function collectSameElements(collectionA, objectB) {
-    let result = [];
-    for (let item of collectionA) {
-        if (includes(objectB.value, item.key)) {
-            result.push(item.key);
-        }
-    }
-    return result;
+    return collectionA.filter((value,index,arr)=>objectB.value.includes(value.key)).map(value=>value.key);    
 }
