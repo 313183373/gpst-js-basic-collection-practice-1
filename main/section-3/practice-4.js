0289d744d7355c7df715fee38b9cf680aa8c5ef6
313183373 <313183373@qq.com>
2017-08-18 15:21:20 +0800

    add map and reduce

--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -12,3 +12,27 @@ module.exports = function createUpdatedCollection(collectionA, objectB) {
         return {key:value[0],count:value[1]};
     });
 }
+
+function map(arr,callback){
+    let temp=new Array(arr.length);
+    for(let i=0;i<arr.length;i++){
+        temp[i]=callback(arr[i],i,arr);
+    }
+    return temp;
+}
+
+function reduce(arr,callback,pre){
+    pre=arr[0];
+    for(let i=1;i<arr.length;i++){
+        pre=callback(pre,arr[i],i,arr);
+    }
+    return pre;
+}
+
+let arr=[1,2,3,4,5]
+console.log(map(arr,function(value,index,arr){
+    return value+1;
+}))
+console.log(reduce(arr,function(pre,now,index,arr){
+    return pre*2+now;
+}));
